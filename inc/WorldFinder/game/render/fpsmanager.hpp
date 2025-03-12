5b76e917919cff02316b6cd0df9a1a59eb1e35ce
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-12 22:12:43 +0800

    目前的帧渲染速率管理器，其实就是冻结线程 挺烂的，先凑合用，不支持 逻辑渲染速率分离

--- inc/WorldFinder/game/render/fpsmanager.hpp
+++ inc/WorldFinder/game/render/fpsmanager.hpp
@@ -2,22 +2,26 @@
 #include <thread>
 #include <chrono>
 
-namespace game::render {
+namespace wf::game::render {
     class FpsManager {
     private:
         std::chrono::high_resolution_clock::time_point t1;
         std::chrono::high_resolution_clock::time_point t2;
         float dt;                   // 每帧时间（秒）
         float target_frame_time;    // 目标帧间隔（秒）
+        bool isEnabled = true;
     public:
         explicit FpsManager(float target_fps) {
             target_frame_time = 1.0f / target_fps;
             dt = 0.0f;
+            if (target_fps < 0) { isEnabled = false; } // 如果目标帧时间小于等于0，禁用fps manager
         }
         void start() {
+            if (!isEnabled) { return; }
             t1 = std::chrono::high_resolution_clock::now();
         };
         void stop() {
+            if (!isEnabled) { return; }
             t2 = std::chrono::high_resolution_clock::now();
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
             dt = duration / 1000.0f;  // 转换为秒
