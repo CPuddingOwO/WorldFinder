6c4bdb064ba93c44c1b908ba2fbcba59d0b41c7e
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-12 22:16:22 +0800

    把Ecs的构造 移到了 Game类中

--- inc/WorldFinder/App.hpp
+++ inc/WorldFinder/App.hpp
@@ -4,42 +4,36 @@
 #include <WorldFinder/di/DependencyInjector.hpp>
 #include <WorldFinder/game/ecs/world.hpp>
 #include <WorldFinder/game/render/sdl.hpp>
-#include <utility>
 
-namespace WorldFinder {
+#include "game/Game.hpp"
+
+
+namespace wf {
     struct AppOptions {
         std::string title;
+        bool isRunning = true;
         int width = 0, height = 0;
-        bool isRunning = false;
 
         AppOptions& setTitle(std::string t) {
             this->title = std::move(t);
             return *this;
         }
-
         AppOptions& setWidth(int w) {
             this->width = w;
             return *this;
         }
-
         AppOptions& setHeight(int h) {
             this->height = h;
-            return *this;
-        }
-
-        AppOptions& setIsRunning(bool flag) {
-            this->isRunning = flag;
             return *this;
         }
     };
 
     class App {
-    private:
         AppOptions options;
         std::shared_ptr<di::DependencyInjector> injector{};
 
-        std::shared_ptr<game::ecs::World> ecs;
         std::shared_ptr<game::render::sdl::Graphics> gfx;
+        std::shared_ptr<wf::game::Game> game;
     public:
         App(const std::shared_ptr<di::DependencyInjector>& injector, const AppOptions& options);
         ~App();
--- src/App.cpp
+++ src/App.cpp
@@ -3,13 +3,17 @@
 #include <WorldFinder/game/render/sdl.hpp>
 #include <WorldFinder/game/render/fpsmanager.hpp>
 
-using namespace game;
+#include <WorldFinder/game/entity/Player.hpp>
 
-namespace WorldFinder {
+#include "WorldFinder/game/Game.hpp"
+
+
+using namespace wf::game::render;
+
+namespace wf {
     App::App(const std::shared_ptr<di::DependencyInjector>& injector, const AppOptions& options) {
         this->injector = injector;
         this->options = options;
-
         this->init();
     }
 
@@ -18,38 +22,29 @@ namespace WorldFinder {
     bool App::init() {
         spdlog::info("Initializing app");
         spdlog::trace("w: {}, h: {}", options.width, options.height );
-        { // Construct graphics
-//            auto inj = std::make_shared<di::DependencyInjector>();
-            auto op = render::sdl::GraphicsOptions();
+        {   // 构造 Graphics: gfx 图形类
+            auto op = sdl::GraphicsOptions();
             op.title = this->options.title;
             op.size = {this->options.width, this->options.height};
             op.isBorderless = false;
             op.isVsync = true;
-            this->gfx = std::make_shared<render::sdl::Graphics>(nullptr, op);
+            this->gfx = std::make_shared<sdl::Graphics>(nullptr, op);
             gfx->setScale(3, 3);
         }
-        { // Construct ecs world
+        {   // 构造 Game 实例
+            // 并 注入 Graphics: gfx 到 Game 实例中
             auto inj = std::make_shared<di::DependencyInjector>();
-            inj->RegisterDependency<render::sdl::Graphics>(this->gfx);
-            auto op = game::ecs::WorldOptions();
-            ecs = std::make_shared<game::ecs::World>(inj, op);
+            inj->RegisterDependency<sdl::Graphics>(this->gfx);
+            auto op = game::GameOptions();
+            game = std::make_shared<game::Game>(inj, op);
         }
-
-        ecs->AddEntity("player")
-            .set<game::ecs::component::Position>({64, 16, 0})
-            .set<game::ecs::component::Velocity>({2, 2})
-            .set<game::ecs::component::Collision>({32, 32});
-        ecs->AddEntity("test1")
-            .set<game::ecs::component::Position>({32, 16, 0})
-            .set<game::ecs::component::Velocity>({0, 0})
-            .set<game::ecs::component::Collision>({32, 32});
         return false;
     }
 
     bool App::run() {
         spdlog::info("Running app");
         while(options.isRunning) {
-            injector->GetDependency<render::FpsManager>()->start();
+            injector->GetDependency<FpsManager>()->start();
             {
                 SDL_Event event;
                 while (SDL_PollEvent(&event)) {
@@ -57,38 +52,29 @@ namespace WorldFinder {
                         this->options.isRunning = false;
                     if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(this->gfx->getWindow()))
                         this->options.isRunning = false;
-
-                    auto player = ecs->GetWorld().entity("player");
-                    if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_D) {
-                        auto vel = player.get<game::ecs::component::Velocity>();
-                        player.set<game::ecs::component::Velocity>({vel->x + 0.75, vel->y});
-                    }
-                    if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_A) {
-                        auto vel = player.get<game::ecs::component::Velocity>();
-                        player.set<game::ecs::component::Velocity>({vel->x - 0.75, vel->y});
-                    }
-                    if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_W) {
-                        auto vel = player.get<game::ecs::component::Velocity>();
-                        player.set<game::ecs::component::Velocity>({vel->x, vel->y - 0.75});
-                    }
-                    if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_S) {
-                        auto vel = player.get<game::ecs::component::Velocity>();
-                        player.set<game::ecs::component::Velocity>({vel->x, vel->y + 0.75});
-                    }
                 }
 
-            }
+                auto player = injector->GetDependency<game::entity::Player>();
+                auto state = SDL_GetKeyboardState(nullptr);
+                if (state[SDL_SCANCODE_D])      { player->go_right(); }
+                if (state[SDL_SCANCODE_A])      { player->go_left(); }
+                if (state[SDL_SCANCODE_SPACE])  { player->jump(); }
 
-            for (size_t i = 0; i < 32; i++) {
-                gfx->addRect({0, 8*i}, {256, 1}, {58, 90, 64, 255}, false); // 红色水平线段，起始位置 (10, 20)，长度 200
-                gfx->addRect({8*i, 0}, {1, 128+64}, {58, 90, 64, 255}, false); // 绿色垂直线段，起始位置 (20, 10)，长度 150
             }
+            auto player = injector->GetDependency<game::entity::Player>();
+
+//            for (size_t i = 0; i < 32; i++) {
+//                gfx->addRect({0, 8*i}, {256, 1}, {58, 90, 64, 255}, false); // 红色水平线段，起始位置 (10, 20)，长度 200
+//                gfx->addRect({8*i, 0}, {1, 128+64}, {58, 90, 64, 255}, false); // 绿色垂直线段，起始位置 (20, 10)，长度 150
+//            }
+            // gfx->addRect({0, 128}, {720, 1}, {58, 90, 64, 255}, false); // 红色水平线段，起始位置 (10, 20)，长度 200
 
 //            gfx->addText(std::format("FPS: {}", injector->GetDependency<render::FpsManager>()->getDelta()), {10, 10});
-            ecs->GetWorld().progress(15.0f);
+
+            this->game->Update();
 
             gfx->onRender();
-            injector->GetDependency<render::FpsManager>()->stop();
+            injector->GetDependency<FpsManager>()->stop();
         }
         return false;
     }
