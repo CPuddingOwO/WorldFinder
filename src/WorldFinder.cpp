2f3e7d552d322decfcd65df2bdd380fdd5e11a0c
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-12 22:31:29 +0800

    阿巴阿巴

--- src/WorldFinder.cpp
+++ src/WorldFinder.cpp
@@ -4,23 +4,22 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 #include <WorldFinder/game/render/fpsmanager.hpp>
+#include <SDL3/SDL_main.h>
 
 
-using namespace WorldFinder;
+using namespace wf;
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[]) {
     spdlog::set_level(spdlog::level::trace);
     spdlog::set_pattern("%^[%H:%M:%S][%L] %v%$");
-
     auto options = AppOptions();
     options.setTitle("World Finder")
-           .setWidth(720)
-           .setHeight(480)
-           .setIsRunning(true);
+           .setWidth(720*2)
+           .setHeight(480*2);
 
     auto injector = std::make_shared<di::DependencyInjector>();
 
-    auto fpsmanager = std::make_shared<game::render::FpsManager>(60.0f);
+    auto fpsmanager = std::make_shared<game::render::FpsManager>(-1);
     injector->RegisterDependency<game::render::FpsManager>(fpsmanager);
 
     try {
