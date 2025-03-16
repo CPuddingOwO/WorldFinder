567e992dd780d311e188812b34d08dc949aae156
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-16 22:18:26 +0800

    注入一些东西到ECS

--- inc/WorldFinder/game/Game.hpp
+++ inc/WorldFinder/game/Game.hpp
@@ -3,6 +3,8 @@
 #include <WorldFinder/game/ecs/world.hpp>
 #include <WorldFinder/game/scene/Scene.hpp>
 
+#include "WorldFinder/Input.hpp"
+
 namespace wf::game {
     using namespace wf::err;
     using namespace wf::game::scene;
@@ -17,6 +19,7 @@ namespace wf::game {
         Quitting = 4,
 
     };
+
     struct GameOptions {
         GameState state = GameState::Null;
         Scene *scene = nullptr;
@@ -25,8 +28,10 @@ namespace wf::game {
     class Game {
         GameState state;
         GameOptions options;
+        std::shared_ptr<input::KeyboardStats> keyboard_stats;
         std::shared_ptr<di::DependencyInjector> injector{};
         std::shared_ptr<World> ecs;
+        std::shared_ptr<Graphics> gfx;
     public:
         Game(const std::shared_ptr<di::DependencyInjector>& injector, const GameOptions& options);
         ~Game() {this->Destroy();};
--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -1,46 +1,64 @@
 #include <WorldFinder/game/Game.hpp>
 #include <WorldFinder/game/entity/Player.hpp>
 
+#include "WorldFinder/game/ecs/component.hpp"
+
 namespace wf::game {
     Game::Game(const std::shared_ptr<di::DependencyInjector> &injector, const GameOptions &options): state() {
         this->injector = injector;
         this->options = options;
+        this->keyboard_stats = injector->GetDependency<input::KeyboardStats>();
         {
             // 构造 EcsWorld 实例
             // 并注入 Graphics gfx 到 EcsWorld
             auto inj = std::make_shared<di::DependencyInjector>();
             inj->RegisterDependency<Graphics>(injector->GetDependency<Graphics>());
+            inj->RegisterDependency<input::KeyboardStats>(keyboard_stats);
             auto op = WorldOptions();
             this->ecs = std::make_shared<World>(inj, op);
         }
+        this->gfx = injector->GetDependency<Graphics>();
     }
 
     Result Game::Initialize() {
+        // 设置Flecs的WebExplorer`
         ecs->GetWorld().import<flecs::stats>();
         ecs->GetWorld().set<flecs::Rest>({27750});
 
-        auto player =std::make_shared<entity::Player>("Player", &ecs->GetWorld());
-        player->init_component();
-        injector->RegisterDependency<entity::Player>(player);
+        {
+            // 初始化玩家
+            auto player = ecs->AddEntity("Player")
+                 .set<component::Sprite>({{16, 16}})
+                 .set<component::Position>({0, 32})
+                 .set<component::Velocity>({3, 0})
+                 .set<component::Drag>({0.90});
+            auto p = std::make_shared<entity::Player>(player);
+            this->injector->RegisterDependency<entity::Player>(p);
+
+        }
         return Result();
     }
 
     Result Game::HandleEvent() {
+        // 不知道这个后续还要不要
         return Result();
     }
 
-    Result Game::Update() {
-        ecs->Update();
+    Result Game::Update() { // 暂不考虑 Const
+        ecs->Update(); // 更新ecs
         return Result();
     }
 
 
     Result Game::Render() {
+        // 将缓冲区的数据渲染到屏幕
+        gfx->onRender();
         return Result();
     }
 
 
     Result Game::Destroy() {
+        // 销毁自身 (当前Game对象)
         return Result();
     }
 
