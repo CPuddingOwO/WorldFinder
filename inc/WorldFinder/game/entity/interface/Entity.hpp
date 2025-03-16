e1b68524c106f9c0f7f53663060a76b148bdffad
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-16 22:16:14 +0800

    增删改查 一些没用的 暂时用不上的抽象接口删掉

--- inc/WorldFinder/game/entity/interface/Entity.hpp
+++ inc/WorldFinder/game/entity/interface/Entity.hpp
@@ -1,11 +1,10 @@
 #pragma once
 #include <flecs.h>
 
-namespace wf::game::entity {
+namespace wf::game::entity::interface {
     class Entity {
     public:
         virtual ~Entity() = default;
-        virtual void init_component() = 0;
     protected:
         flecs::entity entity;
     };
