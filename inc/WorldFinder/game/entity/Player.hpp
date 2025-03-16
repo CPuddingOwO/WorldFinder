57bf0ae7073dafdb6519dfa6f1d2fd4f6d96a0e0
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-16 22:16:24 +0800

    一样的，删掉没用的接口

--- inc/WorldFinder/game/entity/Player.hpp
+++ inc/WorldFinder/game/entity/Player.hpp
@@ -1,34 +1,17 @@
 #pragma once
 
-#include <WorldFinder/game/entity/interface/IEntity.hpp>
-#include <WorldFinder/game/entity/interface/IMove.hpp>
-#include <WorldFinder/game/entity/interface/IPhysics.hpp>
-
+#include <WorldFinder/game/entity/interface/Entity.hpp>
 #include <WorldFinder/game/ecs/world.hpp>
 
-#include <string>
-
 using namespace wf::game::entity::interface;
-using namespace wf::game::ecs::component;
+// using namespace wf::game::ecs::component;
 
 namespace wf::game::entity {
-    class Player : public Entity, public IMove, public IPhysics {
+    class Player : public Entity{
     private:
-        std::string name;
     public:
-        void init_component() override;
-        void init_physics_component() override;
-        void init_move_component() override;
-
-        Player(std::string name, flecs::world* world);
-        void jump() override;
-        void go_left() override;
-        void go_right() override;
-
-        bool is_touch_floor() override;
-        bool is_touch_wall() override;
-        bool is_touch_ceiling() override;
-
-        bool will_touch_floor() override;
+        explicit Player(flecs::entity e) {
+            this->entity = e;
+        }
     };
 }
