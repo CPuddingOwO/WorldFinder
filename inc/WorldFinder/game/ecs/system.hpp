50cb187467680e7c8ab016c51a07fc67374e5ad8
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-17 20:27:24 +0800

    改了一大堆系统

--- inc/WorldFinder/game/ecs/system.hpp
+++ inc/WorldFinder/game/ecs/system.hpp
@@ -5,10 +5,11 @@
 namespace wf::game::ecs::system {
     using namespace game::ecs::component;
 
-    void InputSystem(flecs::entity e, Velocity& vel);
-    void AccelerateSystem(Velocity& vel, const Acceleration& acc);
+    void InputSystem(flecs::entity e, ImpulseAcceleration& i_acc);
+    void AccelerateSystem(const flecs::entity& e, Velocity& vel, ContinuousAcceleration& c_acc, ImpulseAcceleration& i_acc);
     void DragSystem(Velocity& vel, const Drag& drag);
-    void MovementSystem(Position& pos, Velocity& vel);
+    void MovementSystem(Position& pos, const Velocity& vel);
+    void GravitySystem(const Gravity& gra, const Mass& mass, ContinuousAcceleration& c_acc, const Position& pos);
 
     void RenderSystem(flecs::entity e,const Position& pos, const Sprite& sprite);
 }
--- src/game/ecs/system.cpp
+++ src/game/ecs/system.cpp
@@ -5,22 +5,31 @@
 #include "WorldFinder/Input.hpp"
 
 namespace wf::game::ecs::system {
-    void AccelerateSystem(Velocity& vel, const Acceleration& acc) {
-        // if (vel.x != 0) vel.x += acc.x;
-        // if (vel.y != 0) vel.y += acc.y;
+    void AccelerateSystem(const flecs::entity& e, Velocity& vel, ContinuousAcceleration& c_acc, ImpulseAcceleration& i_acc) {
+        // const Acceleration acc = {c_acc.x + i_acc.x, c_acc.y + i_acc.y};
+        const Acceleration acc = {c_acc.x + i_acc.x, c_acc.y + i_acc.y};
+        if (vel.cur.x < vel.max.x) {
+            if (vel.cur.x + acc.x > vel.max.x) vel.cur.x = vel.max.x;
+            else vel.cur.x += acc.x;
+        }
+        if (vel.cur.y < vel.max.y) {
+            if (vel.cur.y + acc.y > vel.max.y) vel.cur.y = vel.max.y;
+            else vel.cur.y += acc.y;
+        }
+        i_acc = {0, 0};
     }
 
     void DragSystem(Velocity& vel, const Drag& drag) {
-        double threshold = 0.05;
+        constexpr double threshold = 0.02;
 
-        if (std::abs(vel.x) < threshold ) vel.x = 0; else vel.x *= drag.value;
-        if (std::abs(vel.y) < threshold ) vel.y = 0; else vel.y *= drag.value;
+        if (std::abs(vel.cur.x) < threshold ) vel.cur.x = 0; else vel.cur.x *= drag.friction;
+        if (std::abs(vel.cur.y) < threshold ) vel.cur.y = 0; else vel.cur.y *= drag.friction;
     }
 
-    void MovementSystem(Position& pos, Velocity& vel) {
-        if (vel.x != 0) pos.x += vel.x;
-        if (vel.y != 0) pos.y += vel.y;
-        // if (vel.x != 0 && vel.y != 0) spdlog::trace("Position: ({}, {}, {})", pos.x, pos.y, pos.z);
+    void MovementSystem(Position& pos, const Velocity& vel) {
+        if (vel.cur.x != 0) pos.x += vel.cur.x;
+        if (vel.cur.y != 0) pos.y += vel.cur.y;
+        // if (vel.cur.x != 0 && vel.cur.y != 0) spdlog::trace("Position: ({}, {}, {})", pos.x, pos.y, pos.z);
     }
 
     void RenderSystem(flecs::entity e,const Position& pos, const Sprite& sprite) {
@@ -29,19 +38,24 @@ namespace wf::game::ecs::system {
         const auto graphics = injector->GetDependency<render::sdl::Graphics>();
         graphics->addRect({pos.x, pos.y}, {sprite.size.x, sprite.size.y}, {52, 78, 65, 255}, true);
         graphics->addRect({pos.x, pos.y}, {4, 4}, {255, 255, 0, 255}, true);
-        // graphics->addText(std::format("ID: {}", e.name().c_str()), {pos.x-(sprite.size.x/2), pos.y-col.y-10});
-        // graphics->addText(std::format("V: ({:.4f}, {:.4f})", vel.x, vel.y), {pos.x, pos.y});
-        // graphics->addText(std::format("P: ({:.4f}, {:.4f})", pos.x, pos.y), {pos.x, pos.y+10});
+        // graphics->addText(std::format("ID: {}", e.name().c_str()), {pos.x-(sprite.size.x/2), pos.y-sprite.size.y-10});
+        // graphics->addText(std::format("V: ({:.4f}, {:.4f})", vel.cur.x, vel.cur.y), {pos.x, pos.y});
+        // graphics->addText(std::format("P: ({:.4f}, {:.4f})", pos.x, pos.y), {pos.x, pos.y+10}, {255, 255, 255, 255}, true);
     }
 
-    void InputSystem(flecs::entity e, Velocity& vel) {
+    void InputSystem(flecs::entity e, ImpulseAcceleration& i_acc) {
         const auto* injector = static_cast<di::DependencyInjector*>(e.world().get_ctx());
         const auto input = injector->GetDependency<input::KeyboardStats>();
 
-        if (input->is_pressed(input::key::W)) vel.y -= 0.2f;
-        if (input->is_pressed(input::key::S)) vel.y += 0.2f;
-        if (input->is_pressed(input::key::A)) vel.x -= 0.2f;
-        if (input->is_pressed(input::key::D)) vel.x += 0.2f;
+        if (input->is_pressed(input::key::W)) i_acc.y = -0.2f;
+        if (input->is_pressed(input::key::S)) i_acc.y = 0.2f;
+        if (input->is_pressed(input::key::A)) i_acc.x = -0.2f;
+        if (input->is_pressed(input::key::D)) i_acc.x = 0.2f;
+    }
+
+    void GravitySystem(const Gravity& gra, const Mass& mass, ContinuousAcceleration& c_acc, const Position& pos) {
+        if (pos.y < 256) c_acc.y = mass.m * gra.g;
+        else c_acc.y = 0;
     }
 
 }
