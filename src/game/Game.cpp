e72be63e8d1f110981e9e09f61ddcd0e5e88c2f1
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2025-03-22 06:17:42 +0800

    修复网格绘制

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -55,6 +55,8 @@ namespace wf::game {
 
 
     Result Game::Render() {
+
+        glm::vec<4, int> col = {110, 139, 116, 255};
         {
             // 绘制网格
             short blod_grid_spacing = 0;
@@ -62,28 +64,29 @@ namespace wf::game {
             const glm::vec<2, int> grid_size = {options.screen_size.x/reg_grid_spacing, options.screen_size.y/reg_grid_spacing};
             for (int i = 0; i < grid_size.x; i+= 1) { // 绘制横向网格
                 if (blod_grid_spacing == 5) {
-                    gfx->addRect({0+i*reg_grid_spacing-1, 0}, {3, options.screen_size.y}, {0, 0, 0, 255}, false);
+                    gfx->addRect({0+i*reg_grid_spacing-1, 0}, {3, options.screen_size.y}, col, true);
                     blod_grid_spacing = 0;
                 } else {
-                    gfx->addRect({0+i*reg_grid_spacing, 0}, {1, options.screen_size.y}, {0, 0, 0, 255}, false);
+                    gfx->addRect({0+i*reg_grid_spacing, 0}, {1, options.screen_size.y}, col, true);
                 }
                 blod_grid_spacing +=1;
             }
             blod_grid_spacing = 0;
             for (int i = 0; i < grid_size.y; i+= 1) {
                 if (blod_grid_spacing == 5) { // 绘制纵向网格
-                    gfx->addRect({0, 0+i*reg_grid_spacing-1}, {options.screen_size.x, 3}, {0, 0, 0, 255}, false);
+                    gfx->addRect({0, 0+i*reg_grid_spacing-1}, {options.screen_size.x, 3}, col, true);
                     blod_grid_spacing = 0;
                 } else {
-                    gfx->addRect({0, 0+i*reg_grid_spacing}, {options.screen_size.x, 1}, {0, 0, 0, 255}, false);
+                    gfx->addRect({0, 0+i*reg_grid_spacing}, {options.screen_size.x, 1}, col, true);
                 }
                 blod_grid_spacing +=1;
             }
-            gfx->addRect({0, 256-1}, {options.screen_size.x, 3}, {128, 0, 0, 255}, false);
+            gfx->addRect({0, 256-1}, {options.screen_size.x, 3}, {117, 49, 23, 255}, true);
+            gfx->submit();
         }
 
         // 将缓冲区的数据渲染到屏幕
-        gfx->onRender();
+        gfx->render();
         return Result();
     }
 
